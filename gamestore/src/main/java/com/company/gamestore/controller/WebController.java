b486bbce39c434bbabbda0e4a242021655ef0cde
Bryan Tineo <brillant16.gcv@gmail.com>
2023-05-29 21:36:13 -0500

    Up to Date! continuing Working

--- gamestore/src/main/java/com/company/gamestore/controller/WebController.java
+++ gamestore/src/main/java/com/company/gamestore/controller/WebController.java
@@ -1,10 +1,11 @@
 package com.company.gamestore.controller;
 
+import com.company.gamestore.model.Console;
 import com.company.gamestore.model.Game;
+import com.company.gamestore.model.Tshirt;
 import com.company.gamestore.service.ServiceLayer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -19,12 +20,12 @@ public class WebController {
 
     @GetMapping("/")
     public String returnMain(){
-        return "index";
+        return "./MainPage/index";
     }
 
     @GetMapping("/checkout")
     public String returnCheckout(){
-        return "checkout";
+        return "./CheckoutPage/checkout";
     }
 
     @GetMapping("/search")
@@ -34,37 +35,37 @@ public class WebController {
 
     @GetMapping("/admin")
     public String returnAdmin(){
-        return "ADMIN";
+        return "./AdminLogin/ADMIN";
     }
 
     @GetMapping("/createGame")
     public String returnCreateGame(){
-        return "createGame";
+        return "./Game/createGame";
     }
 
     @GetMapping("/createConsole")
     public String returnCreateConsole(){
-        return "createConsole";
+        return "./Console/createConsole";
     }
 
     @GetMapping("/createTshirt")
     public String returnCreateTshirt(){
-        return "createT-shirt";
+        return "./TShirt/createT-shirt";
     }
 
 
-    @GetMapping("/searchGames")
-    public String returnSearch(Model model){
+    @GetMapping("/GameCatalog")
+    public String returnSearchGame(Model model){
         List<Game> games = serviceLayer.getAllGames(); //This is an example. Implement a method in your service layer that fetches all the games from your database.
         model.addAttribute("games", games);
-        return "searchGame";
+        return "./Game/searchGame";
     }
 
 
 
     //Getting infrormation of the user through form
     @RequestMapping(value = "/saveGame", method = RequestMethod.POST)
-    public ModelAndView save(@ModelAttribute Game game){
+    public ModelAndView saveGame(@ModelAttribute Game game){
         System.out.println("Game from UI =" + game);
 
         serviceLayer.saveGame(game);
@@ -72,8 +73,58 @@ public class WebController {
 
 
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("game_information");
+        modelAndView.setViewName("./Game/game_information");
         modelAndView.addObject("game",game);
         return modelAndView;
     }
+
+    @GetMapping("/ConsoleCatalog")
+    public String returnSearchConsole(Model model){
+        List<Console> consoles = serviceLayer.getAllConsoles(); //This is an example. Implement a method in your service layer that fetches all the games from your database.
+        model.addAttribute("consoles", consoles);
+        return "./Console/searchConsole";
+    }
+    // *******8
+    @RequestMapping(value = "/saveConsole", method = RequestMethod.POST)
+    public ModelAndView saveConsole(@ModelAttribute Console console){
+        System.out.println("Game from UI =" + console);
+
+        serviceLayer.saveConsole(console);
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("./Console/console_information");
+        modelAndView.addObject("console",console);
+        return modelAndView;
+    }
+
+    @GetMapping("/TshirtCatalog")
+    public String returnSearchTShirt(Model model){
+        List<Tshirt> tshirts = serviceLayer.getAllTshirts(); //This is an example. Implement a method in your service layer that fetches all the games from your database.
+        model.addAttribute("tshirts", tshirts);
+        return "./TShirt/searchTshirt";
+    }
+    // *******8
+    @RequestMapping(value = "/saveTshirt", method = RequestMethod.POST)
+    public ModelAndView saveTshirt(@ModelAttribute Tshirt tshirt){
+        System.out.println("Game from UI =" + tshirt);
+
+        serviceLayer.saveTshirt(tshirt);
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("./TShirt/tshirt_information");
+        modelAndView.addObject("tshirt",tshirt);
+        return modelAndView;
+    }
+
+
+
+
+//    Search for all itams (consoles, games, t-shirts)
+//@GetMapping("/search")
+//public String returnSearch(Model model){
+//    List<Console> consoles = serviceLayer.getAllConsoles(); //This is an example. Implement a method in your service layer that fetches all the games from your database.
+//
+//    model.addAttribute("consoles", consoles);
+//    return "searchConsole";
+//}
 }
